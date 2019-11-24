494be4fc1b45a7d94b69dbf6379ac1c97f292d84
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-24 14:47:28 +0100

    Add Serializable Vector3 / Quaternion

---
+++ src/BP-CoreLib/Serializable/SerializableQuaternion.cs
@@ -0,0 +1,35 @@
+﻿using BPCoreLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BPCoreLib.Serializable
+{
+    public class SerializableQuaternion
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+
+        public float W { get; set; }
+
+        public SerializableQuaternion(Quaternion quaternion)
+        {
+            X = quaternion.x;
+            Y = quaternion.y;
+            Z = quaternion.z;
+            W = quaternion.w;
+        }
+
+        public Quaternion ToQuaternion()
+        {
+            return new Quaternion(X, Y, Z, W);
+        }
+
+    }
+}
---
+++ src/BP-CoreLib/Serializable/SerializableVector3.cs
@@ -0,0 +1,38 @@
+﻿using BPCoreLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BPCoreLib.Serializable
+{
+    public class SerializableVector3
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+
+        public SerializableVector3(Vector3 vector3)
+        {
+            X = vector3.x;
+            Y = vector3.y;
+            Z = vector3.z;
+        }
+
+        public Vector3 ToVector3()
+        {
+            return new Vector3(X, Y, Z);
+        }
+
+        public void FromVector3(Vector3 vector3)
+        {
+            X = vector3.x;
+            Y = vector3.y;
+            Z = vector3.z;
+        }
+    }
+}
