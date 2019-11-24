ce85f9e66f2f5c512a1d8eb3d5c81be34b9bfb66
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-24 13:30:18 +0100

    Remove unnessercary 'using'

--- src/BP-CoreLib/Interfaces/IReader.cs
+++ src/BP-CoreLib/Interfaces/IReader.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BPCoreLib.Interfaces
+﻿namespace BPCoreLib.Interfaces
 {
     public interface IReader<Model>
     {
