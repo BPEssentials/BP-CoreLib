d2ee9d7aa4fc7f4a05d500073d6f6555d1c9418a
PLASMAchicken <plasmachickenmc@gmail.com>
2020-09-06 20:27:53 +0200

    Version 1.0.6

--- src/BP-CoreLib/Properties/AssemblyInfo.cs
+++ src/BP-CoreLib/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("BP-CoreLib")]
 [assembly: AssemblyDescription("Contains a list of helpful features for Broke Protocol plugins. Required core libary by a few plugins.")]
 
-[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.6")]
