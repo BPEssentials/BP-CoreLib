4718c0b29033ef507fef8cd13027dfa8c65d336e
Ido <benhaim.ido@gmail.com>
2020-10-24 00:29:15 -0400

    Added fixes from code review
    Changed arrays to accept any IEnumerable or their type

--- src/BP-CoreLib/OptionMenu/ActionLabel.cs
+++ src/BP-CoreLib/OptionMenu/ActionLabel.cs
@@ -25,17 +25,19 @@ namespace BPCoreLib.OptionMenu
 
     internal class OptionMenu
     {
+        public static Dictionary<string, OptionMenu> Menus = new Dictionary<string, OptionMenu>();
+
         private readonly string _title;
         public Dictionary<string, ActionLabel> Actions { get; private set; }
         private readonly LabelID[] _options;
         private readonly string _menuId;
 
-        public OptionMenu(string title, LabelID[] labels, ActionLabel[] actions)
+        public OptionMenu(string title, IEnumerable<LabelID> labels, IEnumerable<ActionLabel> actions)
         {
-            this._title = title;
-            this._options = labels;
-            this.menuId = Guid.NewGuid().ToString();
-            Menus.Add(menuId, this);
+            _title = title;
+            _options = labels.ToArray();
+            _menuId = Guid.NewGuid().ToString();
+            Menus.Add(_menuId, this);
             Actions = new Dictionary<string, ActionLabel>();
             foreach (var action in actions)
             {
@@ -43,23 +45,21 @@ namespace BPCoreLib.OptionMenu
             }
         }
 
-        public static Dictionary<string, OptionMenu> Menus = new Dictionary<string, OptionMenu>();
-
         internal void SendToPlayer(ShPlayer player)
         {
-            var actions = new LabelID[this.Actions.Count];
-            int i = 0;
-            foreach (var action in this.Actions)
+            var actions = new LabelID[Actions.Count];
+            for (var i = 0; i < actions.Length; i++)
             {
+                var action = Actions.ElementAt(i);
                 actions[i++] = new LabelID(action.Value.Name, action.Key);
             }
-            player.svPlayer.SendOptionMenu(_title, player.ID, menuId, _options, actions);
+            player.svPlayer.SendOptionMenu(_title, player.ID, _menuId, _options, actions);
         }
     }
 
     public static class Extension
     {
-        public static void SendOptionMenu(this ShPlayer player, string title, LabelID[] labels, ActionLabel[] actions)
+        public static void SendOptionMenu(this ShPlayer player, string title, IEnumerable<LabelID> labels, IEnumerable<ActionLabel> actions)
         {
             (new OptionMenu(title, labels, actions)).SendToPlayer(player);
         }
